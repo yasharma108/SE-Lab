9ba87b16d493bb0cb060e4bbb28e02b4e617df31
Yash Sharma <yasharma108@gmail.com>
2026-05-01 14:35:57 +0530

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'yash',
-    password: process.env.DB_PASSWORD || 'yash1204',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'fieldsense',
     waitForConnections: true,
     connectionLimit: 10,
